e0cbab4e8ef4fe8bb466a29619cf6916df374a8d
Caleb Mow <immensedrama@gmail.com>
2025-03-14 19:37:06 +0100

    removed blog component

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,51 +1,66 @@
 import React from "react";
+import { useState } from "react";
 
 const blogPosts = [
   {
     title: "Website Launch",
     date: "March 14, 2025",
-    summary: "We're excited to announce the launch of our new website! Stay tuned for more updates as we continue to build and improve.",
+    summary:
+      "We're excited to announce the launch of our new website! Stay tuned for more updates as we continue to build and improve.",
     link: "#",
     span: { row: 2, column: 2 },
   },
   {
     title: "New Features Coming Soon",
     date: "March 20, 2025",
-    summary: "We've been working hard on adding new features to enhance your experience. Stay updated for what's coming next.",
+    summary:
+      "We've been working hard on adding new features to enhance your experience. Stay updated for what's coming next.",
     link: "#",
     span: { row: 1, column: 1 },
   },
   {
     title: "Performance Improvements",
     date: "March 25, 2025",
-    summary: "Our team has made several performance improvements to ensure a smoother experience across all devices.",
+    summary:
+      "Our team has made several performance improvements to ensure a smoother experience across all devices.",
     link: "#",
     span: { row: 1, column: 1 },
   },
   {
     title: "Update on Progress",
     date: "April 2, 2025",
-    summary: "We’ve made great progress and are adding more functionalities. Here's a sneak peek of what’s coming next.",
+    summary:
+      "We’ve made great progress and are adding more functionalities. Here's a sneak peek of what’s coming next.",
     link: "#",
     span: { row: 1, column: 2 },
   },
   {
     title: "Bug Fixes and Optimizations",
     date: "April 10, 2025",
-    summary: "We’ve fixed several bugs and optimized the website’s performance. Read on to see the changes.",
+    summary:
+      "We’ve fixed several bugs and optimized the website’s performance. Read on to see the changes.",
     link: "#",
     span: { row: 1, column: 1 },
   },
   {
     title: "Upcoming Events and Milestones",
     date: "April 15, 2025",
-    summary: "We have some exciting events and milestones lined up. Stay tuned for more details!",
+    summary:
+      "We have some exciting events and milestones lined up. Stay tuned for more details!",
     link: "#",
     span: { row: 1, column: 1 },
   },
 ];
 
 export default function Blog() {
+  const [showPage, setShowPage] = useState(false);
+
+  if (!showPage)
+    return (
+      <div className="flex w-full justify-center items-center">
+        <p className="h2-text">Coming Soon...</p>
+      </div>
+    );
   return (
     <div className="blog-container mx-auto w-full max-w-6xl px-4">
       <h2 className="text-3xl font-bold text-center mb-8">Blog Feed</h2>
